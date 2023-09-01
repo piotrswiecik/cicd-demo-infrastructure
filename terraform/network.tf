e797d5f8c5137bdc8fb1d7de50cf731f645ea336
piotr <piotr.swiecik@gmail.com>
2023-09-01 12:36:59 +0200

    add http port access to jenkins controller

--- terraform/network.tf
+++ terraform/network.tf
@@ -67,6 +67,14 @@ resource "aws_security_group" "jenkins_controller" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    description = "Allow HTTP from anywhere for Jenkins GUI"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     description     = "Allow access to Jenkins agent listener port"
     from_port       = 50000
